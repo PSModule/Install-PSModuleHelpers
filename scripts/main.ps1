e52ba4ed83c00a6afe2adacbf03d76c6d5883170
Marius Storhaug <marstor@hotmail.com>
2025-03-05 17:56:01 +0100

    Fix error handling when removing obsolete Helpers module directory

--- scripts/main.ps1
+++ scripts/main.ps1
@@ -6,7 +6,7 @@ param()
 $PSModulePath = $env:PSModulePath -split [System.IO.Path]::PathSeparator | Select-Object -First 1
 Remove-Module -Name Helpers -Force -ErrorAction SilentlyContinue
 Get-Command -Module Helpers | ForEach-Object { Remove-Item -Path function:$_ -Force }
-Get-Item -Path "$PSModulePath/Helpers/999.0.0" | Remove-Item -Recurse -Force
+Get-Item -Path "$PSModulePath/Helpers/999.0.0" -ErrorAction SilentlyContinue | Remove-Item -Recurse -Force
 $modulePath = New-Item -Path "$PSModulePath/Helpers/999.0.0" -ItemType Directory -Force | Select-Object -ExpandProperty FullName
 Copy-Item -Path "$PSScriptRoot/Helpers/*" -Destination $modulePath -Recurse -Force
 LogGroup 'Importing module' {
