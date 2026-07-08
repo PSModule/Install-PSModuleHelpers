22e7c03d61d8319363c506c55b620335dd4a4254
Marius Storhaug <marstor@hotmail.com>
2026-07-08 03:06:38 +0200

    Use Join-Path for the version folder and import the fresh copy by manifest path
    Addresses review feedback: build the install path with Join-Path (cross-platform separators) and import the just-installed module by its manifest path so it is not shadowed by another version already on PSModulePath.

--- src/Helpers/Helpers.psm1
+++ src/Helpers/Helpers.psm1
@@ -321,10 +321,12 @@ function Install-PSModule {
         $moduleVersion = '999.0.0'
     }
     $PSModulePath = $env:PSModulePath -split [System.IO.Path]::PathSeparator | Select-Object -First 1
-    $codePath = New-Item -Path "$PSModulePath/$moduleName/$moduleVersion" -ItemType Directory -Force | Select-Object -ExpandProperty FullName
+    $codePath = New-Item -Path (Join-Path -Path $PSModulePath -ChildPath $moduleName -AdditionalChildPath $moduleVersion) -ItemType Directory -Force | Select-Object -ExpandProperty FullName
     Copy-Item -Path "$Path/*" -Destination $codePath -Recurse -Force
     Write-Host '::group::Importing module'
-    Import-Module -Name $moduleName -Verbose
+    # Import the freshly-installed copy explicitly by its manifest path so it is not shadowed
+    # by another version of the same module that may already be on $env:PSModulePath.
+    Import-Module -Name (Join-Path -Path $codePath -ChildPath "$moduleName.psd1") -Verbose
     Write-Host '::endgroup::'
     if ($PassThru) {
         return $codePath
